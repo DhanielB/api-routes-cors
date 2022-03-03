5fa49f432d7c5c6feb1f8e71ae7b963845966d15
dhanielB <dhanielbrandao2@gmai>
2022-03-03 18:39:25 -0300

    main

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -15,5 +15,7 @@ export default async function handler(req, res) {
   await cors(req, res)
 
   // Rest of the API logic
-  res.json({ message: 'Hello Everyone!' })
+  const dynamicDate = Date.now().GMTtoString()
+
+  res.json({ date: dynamicDate })
 }
