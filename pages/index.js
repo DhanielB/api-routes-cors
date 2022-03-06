fc2aabe735d5542361fe9c44d87570fbf4521382
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-03-05 22:29:39 -0300

    main

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-export default function Index() {
+export default function Index({ data }) {
   return (
+    <h1>{data}</h1>
     <p>
       To test the CORS route, open the console in a new tab on a different
       domain and make a POST / GET / OPTIONS request to <b>/api/cors</b>. Using
       a different method from those mentioned will be blocked by CORS
     </p>
   )
+}
+
+export async function getServerSideProps() {
+  data = "test"
+  return { props: { data } }
 }
